fbb78a0161525123c1e6dac93658e0686837741c
Somewhere <likeaboss13371@gmail.com>
2024-01-01 20:43:39 +0200

    Auth And Home Components Added

---
+++ src/pages/Login.js
@@ -0,0 +1,31 @@
+import CustomInput from "components/CustomInput";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+        <h3 className="text-center">Login</h3>
+        <p className="text-center">Login to Your Account to Continue</p>
+        <form action="">
+          <CustomInput type="text" label="Email Address" id="email" />
+          <CustomInput type="password" label="Password" id="pass" />
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
